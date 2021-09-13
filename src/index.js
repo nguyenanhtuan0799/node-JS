66e38dc9f4273da659940278003e9305716144c4
nguyenanhtuan0799 <nguyenanhtuan0799@gmail.com>
2021-09-13 15:05:26 +0700

    install 3 library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
@@ -7,31 +6,31 @@ const app = express();
 const port = 3000;
 const route = require('./route');
 
-
-
 // chọc vào folder public
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 app.use(morgan('combined'));
 
-
 //template engine
-app.engine("hbs",handlebars({
-  extname:".hbs"
-}));
-app.set('view engine','hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 // console.log("path:", path.join(__dirname,'resources\\views'));
-
 
 //router
 route(app);
 
-
 //xuất ra link
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
